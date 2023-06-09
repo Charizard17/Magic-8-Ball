8c691fc427d8b893aeae51ebd9cb6b0b90880846
Charizard17 <3723esad@gmail.com>
2023-06-09 10:49:03 +0200

    imageView and button created, user reaction with ui

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let ballArray = [
+                UIImage(named: "ball1"),
+                UIImage(named: "ball2"),
+                UIImage(named: "ball3"),
+                UIImage(named: "ball4"),
+                UIImage(named: "ball5")
+            ]
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        
+        imageView.image = ballArray[0]
+        }
 
-
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in:0...4)]
+    }
+    
 }
 
